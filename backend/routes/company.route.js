e146a7442d7013ba7159f58e49d9da29c44cc6e7
vivekkumar5555 <143058399+vivekkumar5555@users.noreply.github.com>
2025-06-20 17:14:30 +0530

    Update company.route.js

--- backend/routes/company.route.js
+++ backend/routes/company.route.js
@@ -5,10 +5,16 @@ import { singleUpload } from "../middlewares/mutler.js";
 
 const router = express.Router();
 
-router.route("/register").post(isAuthenticated,registerCompany);
-router.route("/get").get(isAuthenticated,getCompany);
-router.route("/get/:id").get(isAuthenticated,getCompanyById);
-router.route("/update/:id").put(isAuthenticated,singleUpload, updateCompany);
+// router.route("/register").post(isAuthenticated,registerCompany);
+// router.route("/get").get(isAuthenticated,getCompany);
+// router.route("/get/:id").get(isAuthenticated,getCompanyById);
+// router.route("/update/:id").put(isAuthenticated,singleUpload, updateCompany);
+
+
+router.route("/register").post(registerCompany);
+router.route("/get").get(getCompany);
+router.route("/get/:id").get(getCompanyById);
+router.route("/update/:id").put(updateCompany);
 
 export default router;
 
